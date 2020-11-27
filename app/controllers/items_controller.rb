6b389156314e0247b85ed9149ed366f1b74370b0
kenichimori1127 <71421107+kenichimori1127@users.noreply.github.com>
2020-11-27 18:23:07 +0900

    edit,updateコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,18 +23,18 @@ class ItemsController < ApplicationController
    @user = User.select("nickname")
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   item = Item.find(params[:id])
+  #   if item.update(item_params)
+  #     redirect_to item_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   #  def destroy
   #   @item = Item.find(params[:id])
